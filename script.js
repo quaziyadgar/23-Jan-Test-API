3920010875632482a4bf42cd2ceb7c9a0ebe3ff4
quaziyadgar <quaziyadgar@github.com>
2022-01-24 22:44:56 +0530

    half done

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-var country = document.getElementById('country').value;
-var btn_search = document.getElementById('search');
-
+var btn_search = document.getElementById("search");
+var universities = document.getElementById("universites");
 function getData(){
-    console.log("Started fetching...")
-    url = "http://universities.hipolabs.com/search?country={India}";
+    var country = document.getElementById("country").value;
+    // console.log("Started fetching...")
+    universities.innerText="Started fetching...";
+    
+    url = "http://universities.hipolabs.com/search?country="+country;
+           
     fetch(url).then((response)=>{
         console.log("inside first then");
         return response.json();
     }).then((data)=>{
         console.log("inside second then");
-        console.log(data);
+        for(let i in data){
+            var div = document.createElement('div');
+            div.innerText = data[i].name;
+            universities.appendChild(div);
+
+        }
+        // universities.innerText = data;
+    }).catch(arg=>{
+        universities.innerText = "wrong country"+ arg;
     })
+
 }
 
-getData();
+btn_search.addEventListener('click', getData);
