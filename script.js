20cf8e9d975bf209e33064e3047a4ee1737b7fb5
quaziyadgar <quaziyadgar@github.com>
2022-01-25 17:15:52 +0530

    done

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ function getData(){
     url = "http://universities.hipolabs.com/search?country="+country;
            
     fetch(url).then((response)=>{
-        console.log("inside first then");
+        // console.log("inside first then");
         return response.json();
     }).then((data)=>{
-        console.log("inside second then");
+        // console.log("inside second then");
+        var table = document.createElement('table');
+        var tr = document.createElement('tr');
+        var td0 = document.createElement('th');
+        td0.innerText = "S.No.";
+        var td1 = document.createElement('th');
+        td1.innerText = "Universities";
+        var td2 = document.createElement('th');
+        td2.innerText = "Website";
+            tr.appendChild(td0);
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            table.appendChild(tr);
+            universities.appendChild(table);
         for(let i in data){
-            var div = document.createElement('div');
-            div.innerText = data[i].name;
-            universities.appendChild(div);
+            let tr = document.createElement('tr');
+            let td0 = document.createElement('td');
+            let td1 = document.createElement('td');
+            let td2 = document.createElement('td');
+            td0.innerText = i;
+            td1.innerText = data[i].name;
+            td2.innerText = data[i].web_pages;
+            tr.appendChild(td0);
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            table.appendChild(tr);
+            universities.appendChild(table);
 
         }
         // universities.innerText = data;
     }).catch(arg=>{
-        universities.innerText = "wrong country"+ arg;
-    })
+        universities.innerText = "404 "+ arg;
+    });
 
 }
 
